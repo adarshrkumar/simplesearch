64381aff7f9931127cee0eb8815b93a619685459
adarshrkumar <git@stackblitz.com>
2024-10-14 12:45:01 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express()
+var newSite = 'https://search.samalander.dev'
+
+app.get('/simpleSearch.exe',function(_,res) {
+  res.sendFile(__dirname + '/simpleSearch.html')
+});
+
+app.get('/simpleSearch.crx',function(_,res) {
+  res.sendFile(__dirname + '/simpleSearch-crx.html')
+});
+
+app.get('/setup',function(_,res) {
+  res.sendFile(__dirname + '/setup.html')
+});
+
+app.get('/public*', function(req, res) {
+  let page = req.originalUrl;
+  res.sendFile(__dirname + page);
+});
+
+app.get('*', function(req, res) {
+  let page = req.originalUrl;
+  res.redirect(newSite + page);
+});
+
+app.listen(3000)
