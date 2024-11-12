aaa8a01986026db72f1f4bf753cd5210db0b4e4b
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-11-12 10:46:38 -0800

    update index.js

--- index.js
+++ index.js
@@ -2,26 +2,30 @@ var express = require('express')
 var app = express()
 var newSite = 'https://search.samalander.dev'
 
+app.get('/',function(_,res) {
+  res.sendFile(`${__dirname}/setup`)
+});
+
 app.get('/simpleSearch.exe',function(_,res) {
-  res.sendFile(__dirname + '/simpleSearch.html')
+  res.sendFile(`${__dirname}/simpleSearch.html`)
 });
 
 app.get('/simpleSearch.crx',function(_,res) {
-  res.sendFile(__dirname + '/simpleSearch-crx.html')
+  res.sendFile(`${__dirname}/simpleSearch-crx.html`)
 });
 
 app.get('/setup',function(_,res) {
-  res.sendFile(__dirname + '/setup.html')
+  res.sendFile(`${__dirname}/setup.html`)
 });
 
 app.get('/public*', function(req, res) {
   let page = req.originalUrl;
-  res.sendFile(__dirname + page);
+  res.sendFile(`${__dirname}${page}`);
 });
 
 app.get('*', function(req, res) {
   let page = req.originalUrl;
-  res.redirect(newSite + page);
+  res.sendFile(`${__dirname}${page}`);
 });
 
 app.listen(3000)
